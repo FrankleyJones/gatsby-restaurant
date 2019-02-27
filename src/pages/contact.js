0ebbe29d8a739a12159ba2063bc5c2383ea9eb1c
Frankley <frankleyj.o@gmail.com>
2019-02-27 13:29:42 -0300

    navbar added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,11 +3,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const ContactPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hello from Gatsby!</h1>
+    <h1>This is contact page</h1>
   </Layout>
 )
 
-export default IndexPage
+export default ContactPage
